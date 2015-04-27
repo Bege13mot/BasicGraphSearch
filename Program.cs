e94870628bda94788ce3d0bf76e644c9a173a3ed
Bege13mot <bege13mot@gmail.com>
2015-04-27 23:25:06 +0400

    Dijkstras Search
    Dijkstra's algorithm search implementation

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ namespace BasicGraphSearch
 
                 return Aaron;
             }
+            
 
             /*
              * Поиск в глубину, кажда ветка просматривается конца
@@ -79,7 +80,8 @@ namespace BasicGraphSearch
                     }
                 }
                 return null;
-            }
+            }   
+            
 
             public void PrintGraph(Graph root)
             {
@@ -90,6 +92,89 @@ namespace BasicGraphSearch
                 }
             }
         }
+
+        
+        /*
+            * Алгоритм Дейкстры. Поиск кратчайшего пути во взвешенном графе. 
+            * На основе https://github.com/mburst/dijkstras-algorithm
+            * Алгоритм: Проставляем начальной вершине метку 0, для остальных max of int
+            * Затем ищем вершину с минимальным растоянием и меткой 0. Затем добавляем в список, и удаляем из списка
+            * те вершины, растояние (вес) у которых масимальный, если минимальный, то наоборот добавляем. 
+            * Цикл остановится когда у всех вершин метка 0 или max int. Последний случай значит что граф не связанный.
+            * Сложность - квадратичная.
+            */
+
+        class DijkstrasSearch
+        {
+            Dictionary<char, Dictionary<char, int>> vertices = new Dictionary<char, Dictionary<char, int>>();
+
+            public void AddVertex(char name, Dictionary<char, int> edges)
+            {
+                vertices[name] = edges;
+            }
+
+            public List<char> ShortestPath(char start, char finish)
+            {
+                var previous = new Dictionary<char, char>();
+                var distances = new Dictionary<char, int>();
+                var nodes = new List<char>();
+
+                List<char> path = null;
+
+                foreach (var vertex in vertices)
+                {
+                    if (vertex.Key == start)
+                    {
+                        distances[vertex.Key] = 0;
+                    }
+                    else
+                    {
+                        distances[vertex.Key] = int.MaxValue;
+                    }
+
+                    nodes.Add(vertex.Key);
+                }
+
+                while (nodes.Count != 0)
+                {
+                    nodes.Sort((x, y) => distances[x] - distances[y]);
+
+                    var smallest = nodes[0];
+                    nodes.Remove(smallest);
+
+                    if (smallest == finish)
+                    {
+                        path = new List<char>();
+                        while (previous.ContainsKey(smallest))
+                        {
+                            path.Add(smallest);
+                            smallest = previous[smallest];
+                        }
+
+                        break;
+                    }
+
+                    if (distances[smallest] == int.MaxValue)
+                    {
+                        break;
+                    }
+
+                    foreach (var neighbor in vertices[smallest])
+                    {
+                        var alt = distances[smallest] + neighbor.Value;
+                        if (alt < distances[neighbor.Key])
+                        {
+                            distances[neighbor.Key] = alt;
+                            previous[neighbor.Key] = smallest;
+                        }
+                    }
+                }
+
+                return path;
+            }
+        }
+
+
 
         static void Main(string[] args)
         {
@@ -109,6 +194,19 @@ namespace BasicGraphSearch
             Console.WriteLine(p == null ? "Person not found" : p.Name);
             p = b.BFS(root, "Alex");
             Console.WriteLine(p == null ? "Person not found" : p.Name);
+            
+            Console.WriteLine("\nDijkstra's algorithm\n------");
+            DijkstrasSearch d = new DijkstrasSearch();
+            d.AddVertex('A', new Dictionary<char, int>() { { 'B', 1 }, { 'C', 8 } });
+            d.AddVertex('B', new Dictionary<char, int>() { { 'A', 7 }, { 'F', 2 } });
+            d.AddVertex('C', new Dictionary<char, int>() { { 'A', 8 }, { 'F', 6 }, { 'G', 10 } });
+            d.AddVertex('D', new Dictionary<char, int>() { { 'F', 8 }, { 'H', 3 } });
+            d.AddVertex('E', new Dictionary<char, int>() { { 'H', 5 } });
+            d.AddVertex('F', new Dictionary<char, int>() { { 'B', 2 }, { 'C', 6 }, { 'D', 8 }, { 'G', 9 } });
+            d.AddVertex('G', new Dictionary<char, int>() { { 'C', 4 }, { 'F', 5 }, { 'E', 6 } });
+            d.AddVertex('H', new Dictionary<char, int>() { { 'E', 1 }, { 'F', 3 } });
+            d.ShortestPath('A', 'E').ForEach(x => Console.WriteLine(x));
+
             Console.ReadLine();
         }
     }
